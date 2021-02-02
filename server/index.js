3a96194339a5c62424c4f36e4430a3a76e041ea7
Ayako <ayakosayama1987@gmail.com>
2021-02-02 08:45:14 -0800

    update dotenv

--- server/index.js
+++ server/index.js
@@ -2,14 +2,23 @@ import express from "express"
 import bodyParser from "body-parser"
 import mongooose from "mongoose"
 import cors from "cors"
+// require('dotenv').config()
+
+import dotenv from 'dotenv'
+dotenv.config()
 
 import postRoutes from "./routes/posts.js"
 
 const app = express()
 
+app.get("/", (req, res) => {
+  res.send('Welcome to the api')
+})
+
 app.use("/posts", postRoutes)
 
 const connectionUrl = process.env.ATLAS_URI
+
 const PORT = process.env.PORT || 4000
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
@@ -17,9 +26,6 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors())
 
 
-// app.get("/", (req, res) => {
-//   res.send('Hello World')
-// })
 mongooose.connect(connectionUrl, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
